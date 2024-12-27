cd7728170e8edef7ae5b698cfd34954a3f04125c
Francisco Medina <pacomedtej@gmail.com>
2024-12-26 21:06:00 -0600

    First View, Migrations and Routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('new', function(){
+    return view('create');
+});
+
+Route::get('/view/{id}', function($id){
+    return "You're viewing Note $id";
+});
